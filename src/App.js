33c9f9507593bc6c6e37d00cfe00f7fc37dc82da
Ankur Seth <iankurseth@gmail.com>
2022-09-14 02:26:36 +0530

    Cleaned up * added tailwindcss * deployed to netlify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="h-screen flex bg-green-500 text-white">
+        <h1 className="text-6xl font-bold m-auto">NullCSS</h1>
       </header>
     </div>
   );
